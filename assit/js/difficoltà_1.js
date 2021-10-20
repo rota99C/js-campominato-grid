28911064e6af0af20b2684960c6d224a1b1d9b47
rota99C <rotaboolean@gmail.com>
2021-10-20 19:03:11 +0200

    esercizio completo

--- assit/js/difficoltà_1.js
+++ assit/js/difficoltà_1.js
@@ -1,71 +1,36 @@
 //L'utente indica un livello di difficoltà
 
-//in base al quale viene generata una griglia di gioco quadrata,
-//in cui ogni cella contiene un numero tra quelli compresi in un range:
-//con difficoltà 1 => tra 1 e 100
-//con difficoltà 2 => tra 1 e 81
-//con difficoltà 3 => tra 1 e 49
+let container = document.querySelector('.container')
 
-let contenitore = document.getElementById("contenitore")
+function creaGriglia(numero) {
+    for (let index = 0; index < numero; index++) {
+        let radice = Math.sqrt(numero)
+        container.style.width = radice * 100 + "px"
+        container.style.maxWidth = radice * 100 + "px"
+        const elGriglia = document.createElement('div')
+        elGriglia.className = 'cella'
+        container.appendChild(elGriglia)
+        elGriglia.addEventListener("click", function () {
+            elGriglia.classList.add("azzurro");
+        });
+    }
+}
 
 document.querySelector(".btn1").addEventListener("click", function () {
-    contenitore.className = 'container_1';
+    numQuadrati = 100;
+    creaGriglia(numQuadrati)
+})
 
+document.querySelector(".btn2").addEventListener("click", function () {
+    numQuadrati = 81;
+    creaGriglia(numQuadrati)
 })
 
 
-
-
-//prima_difficoltà
-
-let container_1 = document.querySelector('.container_1')
-
-for (let index = 0; index < 100; index++) {
-    const elGriglia = document.createElement('div')
-    elGriglia.className = 'cella'
-    container_1.append(elGriglia)
-    elGriglia.addEventListener("click", function () {
-        elGriglia.classList.add("azzurro");
-    })
-
-}
-
-
-
-//seconda difficoltà
-
-let container_2 = document.querySelector('.container_2')
-
-for (let index = 0; index < 81; index++) {
-    const elGriglia = document.createElement('div')
-    elGriglia.className = 'cella'
-    container_2.append(elGriglia)
-    elGriglia.addEventListener("click", function () {
-        elGriglia.classList.add("azzurro");
-    })
-
-}
-
-
-//terza difficoltà
-
-let container_3 = document.querySelector('.container_3')
-
-for (let index = 0; index < 49; index++) {
-    const elGriglia = document.createElement('div')
-    elGriglia.className = 'cella'
-    container_3.append(elGriglia)
-    elGriglia.addEventListener("click", function () {
-        elGriglia.classList.add("azzurro");
-    })
-
-}
-
-
-
-//Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
-
-
+document.querySelector(".btn3").addEventListener("click", function () {
+    numQuadrati = 49;
+    creaGriglia(numQuadrati)
+})
 
 
 
